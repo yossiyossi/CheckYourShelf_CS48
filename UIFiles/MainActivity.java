e05041d1591b9dd04dc7b227baa4084a57a06b60
Matthew Maatubang <matthewmaatubang@169-231-83-81.wireless.ucsb.edu>
2018-02-12 14:09:48 -0800

    Additional UI files

---
+++ UIFiles/MainActivity.java
@@ -0,0 +1,40 @@
+package com.androidbegin.buttonclicktutorial;
+ 
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+ 
+public class MainActivity extends Activity {
+	Button button;
+ 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Get the view from activity_main.xml
+		setContentView(R.layout.activity_main);
+ 
+		// Locate the button in activity_main.xml
+		button = (Button) findViewById(R.id.MyButton);
+ 
+		// Capture button clicks
+		button.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+ 
+				// Start NewActivity.class
+				Intent myIntent = new Intent(MainActivity.this,
+						NewActivity.class);
+				startActivity(myIntent);
+			}
+		};
+	}
+ 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+}
